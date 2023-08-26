3fe3c42da7ac6add76b9271d08d45d3755340bbf
武金斗 <wujindou@wujindoudeMacBook-Pro.local>
2023-08-26 10:26:34 +0800

    add service debug

--- tools.py
+++ tools.py
@@ -122,6 +122,87 @@ def unify_char_format(string):
     string = string.lower()                               
     return string
 
-input_mention = '史蒂芬霍金'
-rela_ents = entity_linking(mention2entity_dict, input_mention)
-print('匹配到知识库中的候选实体：', rela_ents)
+def search_triples_by_index(relative_entitis, index, raw_graph_f):
+    """
+    :param relative_entitis: list
+    :param index: dict
+    :param raw_graph: the file-pointer of the raw graph file, and the content need to be post-process
+    :return: list of all the triples relative to the input_triples entitis  双重列表
+    """
+    relative_triples = []
+    for entity in relative_entitis:
+        index_entity = index.get(entity, None)
+        if index_entity:
+            read_index, read_size = index[entity]['start_pos'], index[entity]['length']
+            raw_graph_f.seek(read_index)
+            readresult = raw_graph_f.read(read_size).decode('utf-8')
+            for line in readresult.strip().split('\n'):
+                triple = line.strip().split(' ||| ')
+                relative_triples.append(triple)
+    return relative_triples
+
+from work.config import Word2VecConfig
+from gensim.models import KeyedVectors
+wd_config = Word2VecConfig()
+wd_config.model_path = '/kaggle/input/kbqadata/sgns.target.word-character/sgns.target.word-character'
+def load_word2vec():
+    word2vec_model_path = wd_config.model_path # 词向量文件的位置
+    print('正在预加载word2vec词向量，预计2min', datetime.datetime.now())
+    word2vec_model = KeyedVectors.load_word2vec_format(word2vec_model_path, binary=False, unicode_errors='ignore')
+    print('word2vec词向量加载完毕', datetime.datetime.now())
+    return word2vec_model
+
+def span_question(question, ner_result):
+    """
+    用于答案排序阶段，删去问句中与答案排序无关的信息，如主题词、疑问词等
+    """
+    question = question.replace(ner_result, '').replace('《', '').replace('》', '')
+    for delete_word in ['我想知道','我想请问','请问你','请问','你知道','谁知道','知道','谁清楚','我很好奇','你帮我问问','有没有人看过','有没有人'
+                        '怎么','这个','有多少个','有哪些','哪些','哪个','多少','几个','谁','被谁','还有'
+                        ,'吗','呀','啊','吧','着','的','是','呢','了','？','?','什么']:
+        question = question.replace(delete_word, '')
+
+    return question
+
+span_res = span_question('襄城公主的老公是谁？', '襄城公主')
+print(span_res)
+
+def score_similarity(word2vec_model, string1, string2):
+    """
+    比较两个字符串的相似度，从字符覆盖度、w2v相似度做综合评分，用于答案排序时，问句和三元组关系名的比较
+    :return: 相似度得分
+    """
+    return char_overlap(string1, string2) + word2vec_sim(word2vec_model, string1, string2)
+
+
+def char_overlap(string1, string2):
+    char_intersection = set(string1) & set(string2)
+    char_union = set(string1) | set(string2)
+    return len(char_intersection) / len(char_union)
+
+
+def word2vec_sim(word2vec_model, string1, string2):
+    # 阅读n_similarity的源代码，是对两组词向量分别取平均值并做L2归一化，然后求内积
+    words1 = jieba.cut(string1)
+    words2 = jieba.cut(string2)
+
+    de_seg1 = []
+    de_seg2 = []
+    vocabs =list(word2vec_model.key_to_index.keys())
+    for seg in words1:
+        if seg not in vocabs:
+            _ws = [_w for _w in seg if _w in vocabs]
+            de_seg1.extend(_ws)
+        else:
+            de_seg1.append(seg)
+    for seg in words2:
+        if seg not in vocabs:
+            _ws = [_w for _w in seg if _w in vocabs]
+            de_seg1.extend(_ws)
+        else:
+            de_seg2.append(seg)
+    if de_seg1 and de_seg2:
+        score = word2vec_model.n_similarity(de_seg1, de_seg2)
+    else:
+        score = 0
+    return score
--- utils.py
+++ utils.py
@@ -30,18 +30,19 @@ print('正在加载知识库', datetime.datetime.now())
 forward_KG_f = open(knowledge_graph_path, 'rb')
 print('知识库加载完毕', datetime.datetime.now())
 
-from work.config import Word2VecConfig
-from gensim.models import KeyedVectors
-wd_config = Word2VecConfig()
-wd_config.model_path = '/kaggle/input/kbqadata/sgns.target.word-character/sgns.target.word-character'
-def load_word2vec():
-    word2vec_model_path = wd_config.model_path # 词向量文件的位置
-    print('正在预加载word2vec词向量，预计2min', datetime.datetime.now())
-    word2vec_model = KeyedVectors.load_word2vec_format(word2vec_model_path, binary=False, unicode_errors='ignore')
-    print('word2vec词向量加载完毕', datetime.datetime.now())
-    return word2vec_model
+KGconfig.forward_index_path='/kaggle/input/kbqadata/forward_index.json'
+print('正在加载索引表', datetime.datetime.now())
+forward_index_path = KGconfig.forward_index_path
+with open(forward_index_path, 'r', encoding='utf-8') as f:
+    forward_index = json.loads(f.read())
+print('索引表加载完毕', datetime.datetime.now())
+
 word2vec_model = load_word2vec()
 
 input_mention = '史蒂芬霍金'
 rela_ents = entity_linking(mention2entity_dict, input_mention)
 print('匹配到知识库中的候选实体：', rela_ents)
+
+rel_triples= search_triples_by_index(rela_ents, forward_index, forward_KG_f)
+print('共检索到{}条三元组'.format(len(rel_triples)))
+print('打印20条以内的三元组：')
